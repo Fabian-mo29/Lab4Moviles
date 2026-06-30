5ec882bf9c66662a153dee62b399ad436711a5ed
Fabian Mora Arias <fabian.moraarias@ucr.ac.cr>
2026-06-29 22:04:03 -0600

    fix: Swagger auth, and profile endpoint

--- backend/src/app/api/v1/auth.py
+++ backend/src/app/api/v1/auth.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.domain.schemas.user import (
     UserProfileResponseDTO,
@@ -15,20 +16,21 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 @router.post("/register", status_code=201)
 async def register(
     dto: RegisterRequestDTO,
-    service: UserService = Depends(get_user_service) 
+    service: UserService = Depends(get_user_service)
 ):
     return await service.register(dto)
-    
+
 @router.post("/login", response_model=TokenResponseDTO, status_code=200)
 async def login(
-    dto: LoginRequestDTO,
-    service: UserService = Depends(get_user_service) 
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    service: UserService = Depends(get_user_service)
 ):
+    dto = LoginRequestDTO(email=form_data.username, password=form_data.password)
     return await service.login(dto)
 
 @router.get("/profile", response_model=UserProfileResponseDTO, status_code=200)
 async def get_profile(
     user: CurrentUser,
-    service: UserService = Depends(get_user_service) 
+    service: UserService = Depends(get_user_service)
 ):
-    return await service.get_profile()
+    return await service.get_profile(user.id)
